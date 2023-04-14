71a30637fd4d8b5411cdb8c2c4fd9735b77dc44a
chakibx <chakib_moussaoui@outlook.fr>
2023-04-14 19:28:39 +0200

    main

--- src/main/java/Client/Main.java
+++ src/main/java/Client/Main.java
@@ -1,65 +1,59 @@
 package Client;
 
+import org.Resources.Player;
+import org.Resources.Team;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Bienvenue dans l'application de gestion des équipes de football!\n");
         while (true) {
-            System.out.println("Bienvenue dans l'application de gestion des équipes de football!\n");
             System.out.println("Sélectionnez une option:");
+            System.out.println("1. commencer un match");
             System.out.println("1. Afficher toutes les équipe");
             System.out.println("2. Afficher tous les joueurs");
-            System.out.println("3. Créer une équipe");
-            System.out.println("4. Créer un joueur");
-            System.out.println("5. Ajouter un joueur à une équipe");
-            System.out.println("6. Supprimer un joueur d'une équipe");
-            System.out.println("7. Modifier les statistiques d'un joueur");
-            System.out.println("8. Quitter l'application\n");
+            System.out.println("3. Afficher tous les joueurs d'une équipe");
+            System.out.println("4. Créer une équipe");
+            System.out.println("5. Créer un joueur");
+            System.out.println("6. Ajouter un joueur à une équipe");
+            System.out.println("7. Supprimer un joueur d'une équipe");
+            System.out.println("8. Modifier les statistiques d'un joueur");
+            System.out.println("9. Quitter l'application\n");
             System.out.print("Entrez votre choix (1-6): ");
 
             int choix = scanner.nextInt();
             scanner.nextLine(); // pour consommer le retour à la ligne
 
             switch (choix) {
+                case 1:
+                    TeamRequests.displayAllTeams();
+                    break;
+                case 2:
+                    PlayerRequests.displayAllPlayers();
+                    break;
                 case 3:
-                    System.out.print("Entrez le nom de l'équipe: ");
-                    String nomEquipe = scanner.nextLine();
-                    // Appeler la méthode pour créer une équipe
+                    TeamRequests.displayTeamPlayersFromUserInput();
                     break;
                 case 4:
-                    System.out.print("Entrez le nom du joueur: ");
-                    String nomJoueur = scanner.nextLine();
-                    System.out.print("Entrez l'id de l'équipe du joueur: ");
-                    int ageJoueur = scanner.nextInt();
-                    PlayerRequests player = PlayerRequests.createPlayer();
-                    player.savePlayer();
+                    TeamRequests teamRequests = TeamRequests.createTeam();
+                    teamRequests.saveTeam();
                     break;
                 case 5:
-                    System.out.print("Entrez le nom de l'équipe: ");
-                    nomEquipe = scanner.nextLine();
-                    System.out.print("Entrez le nom du joueur: ");
-                    nomJoueur = scanner.nextLine();
-                    //TeamRequests.addPlayerToTeam();
+                    PlayerRequests playerRequests = PlayerRequests.createPlayer();
+                    playerRequests.savePlayer();
                     break;
                 case 6:
-                    System.out.print("Entrez le nom de l'équipe: ");
-                    nomEquipe = scanner.nextLine();
-                    System.out.print("Entrez le nom du joueur: ");
-                    nomJoueur = scanner.nextLine();
-                    // Appeler la méthode pour supprimer un joueur d'une équipe
+                    TeamRequests.addPlayerToTeam();
                     break;
                 case 7:
-                    System.out.print("Entrez le nom du joueur: ");
-                    nomJoueur = scanner.nextLine();
-                    System.out.print("Entrez la statistique à modifier (ex: 'buts', 'passes décisives'): ");
-                    String statistique = scanner.nextLine();
-                    System.out.print("Entrez la nouvelle valeur: ");
-                    int valeur = scanner.nextInt();
-                    // Appeler la méthode pour modifier les statistiques d'un joueur
+                    //
                     break;
                 case 8:
+                    //
+                    break;
+                case 9:
                     System.out.println("Merci d'avoir utilisé l'application!");
                     System.exit(0);
                     break;
