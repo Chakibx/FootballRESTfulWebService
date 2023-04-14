e5966299bd0023586d306356c1ef5b70c9a1fc69
AyoubCy <ayoubsalah0111@gmail.com>
2023-04-14 02:19:18 +0200

    main player

--- src/main/java/Client/Main.java
+++ src/main/java/Client/Main.java
@@ -1,3 +1,42 @@
+package Client;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Client client = ClientBuilder.newClient();
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter player name: ");
+            String name = scanner.nextLine();
+            System.out.println("Enter player team ID: ");
+            int teamId = scanner.nextInt();
+            scanner.nextLine(); // to consume the new line character after the integer input
+
+            String json = "{\"name\": \"" + name + "\", \"teamId\": " + teamId + "}";
+            Response response = client.target("http://localhost:8080/ws/webapi/players")
+                    .request(MediaType.APPLICATION_JSON)
+                    .post(Entity.json(json));
+
+            String output = response.readEntity(String.class);
+            System.out.println("Output from Server .... \n" + output);
+
+            client.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
 /*package Client;
 
 import java.util.Scanner;
